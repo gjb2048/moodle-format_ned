8f16bd383eba592fada5a9214bca96694fc4b533
Michael Gardener <mgardener@cissq.com>
2017-12-15 22:07:04 -0500

    Backup and restore

--- backup/moodle2/backup_format_ned_plugin.class.php
+++ backup/moodle2/backup_format_ned_plugin.class.php
@@ -26,8 +26,31 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017121500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2017051500.00; // This is Moodle 3.3 (Build: 20170515).
-$plugin->maturity = MATURITY_BETA;
-$plugin->component = 'format_ned';
-$plugin->release = '3.3.1.3';
+/**
+ * Specialised restore for format_ned
+ *
+ * Processes custom options
+ *
+ * @package   format_ned
+ * @category  backup
+ * @developer 2017 Michael Gardener
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class backup_format_ned_plugin extends backup_format_plugin {
+    /**
+     * Returns the format information to attach to section element
+     */
+    protected function define_section_plugin_structure() {
+        $plugin = $this->get_plugin_element(null, $this->get_format_condition(), 'ned');
+        // Create one standard named plugin element (the visible container).
+        $pluginwrapper = new backup_nested_element($this->get_recommended_name(), array('id'),
+            array('name', 'value')
+        );
+        // Connect the visible container ASAP.
+        $plugin->add_child($pluginwrapper);
+        // Set source to populate the data.
+        $pluginwrapper->set_source_table('format_ned', array('sectionid' => backup::VAR_SECTIONID));
+        // Don't need to annotate ids nor files.
+        return $plugin;
+    }
+}
